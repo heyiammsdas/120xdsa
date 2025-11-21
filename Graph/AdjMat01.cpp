664f009ce5befdfd22fca3dabcf4c9d2679b25c1
Madhusudan Das <msdas7476@gmail.com>
2025-11-21 23:32:23 +0530

    Add files via upload

---
+++ Graph/AdjMat01.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm> 
+#include <climits>
+#include <vector> 
+
+using namespace std ;
+
+int main() {
+
+    int vertex , edges ;
+
+    cin>>vertex>>edges ;
+
+    vector < vector < int > > adjMat(vertex , vector<int> (vertex , 0)) ;
+
+    for(int i=0 ; i<edges ; i++){
+
+        int v , u ;
+        cin>>v >> u ;
+
+        adjMat[v][u] = 1 ;
+        adjMat[u][v] = 1 ;
+
+        
+
+
+    }
+
+
+
+    for(int i = 0; i<vertex ; i++){
+        for(int j = 0 ; j<vertex ; j++){
+
+            cout<<adjMat[i][j] <<" " ;
+
+
+        }
+        cout<<endl; 
+        
+
+    }
+
+
+
+}
---
+++ Graph/AdjMat02.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm> 
+#include <climits>
+#include <vector> 
+
+using namespace std ;
+
+int main() {
+
+    int vertex , edges ;
+
+    cin>>vertex>>edges ;
+
+    vector < vector < int > > adjMat(vertex , vector<int> (vertex , 0)) ;
+
+    for(int i=0 ; i<edges ; i++){
+
+        int v , u , weight ;
+        cin>>v >> u  >> weight; 
+
+        adjMat[u][v] = weight ;
+        adjMat[v][u] = weight ;
+
+       
+        
+
+
+    }
+
+
+
+    for(int i = 0; i<vertex ; i++){
+        for(int j = 0 ; j<vertex ; j++){
+
+            cout<<adjMat[i][j] <<" " ;
+
+
+        }
+        cout<<endl; 
+        
+
+    }
+
+
+
+}
